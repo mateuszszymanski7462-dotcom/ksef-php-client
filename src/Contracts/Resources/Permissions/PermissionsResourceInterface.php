8a781396f3df07d230c604064f377c0731de4f7e
N1ebieski <kontakt@intelekt.net.pl>
2025-11-03 14:15:52 +0100

    Add permissions/*/grants endpoints Fixes #30

---
+++ src/Contracts/Resources/Permissions/PermissionsResourceInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N1ebieski\KSEFClient\Contracts\Resources\Permissions;
+
+use N1ebieski\KSEFClient\Contracts\Resources\Permissions\Authorizations\AuthorizationsResourceInterface;
+use N1ebieski\KSEFClient\Contracts\Resources\Permissions\Entities\EntitiesResourceInterface;
+use N1ebieski\KSEFClient\Contracts\Resources\Permissions\EuEntities\EuEntitiesResourceInterface;
+use N1ebieski\KSEFClient\Contracts\Resources\Permissions\Indirect\IndirectResourceInterface;
+use N1ebieski\KSEFClient\Contracts\Resources\Permissions\Persons\PersonsResourceInterface;
+use N1ebieski\KSEFClient\Contracts\Resources\Permissions\Subunits\SubunitsResourceInterface;
+
+interface PermissionsResourceInterface
+{
+    public function persons(): PersonsResourceInterface;
+
+    public function entities(): EntitiesResourceInterface;
+
+    public function authorizations(): AuthorizationsResourceInterface;
+
+    public function idirect(): IndirectResourceInterface;
+
+    public function subunits(): SubunitsResourceInterface;
+
+    public function euEntities(): EuEntitiesResourceInterface;
+}
---
+++ src/Requests/Permissions/Indirect/Grants/GrantsRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N1ebieski\KSEFClient\Requests\Permissions\Indirect\Grants;
+
+use N1ebieski\KSEFClient\Contracts\BodyInterface;
+use N1ebieski\KSEFClient\DTOs\Requests\Permissions\SubjectIdentifierFingerprintGroup;
+use N1ebieski\KSEFClient\DTOs\Requests\Permissions\SubjectIdentifierNipGroup;
+use N1ebieski\KSEFClient\DTOs\Requests\Permissions\SubjectIdentifierPeselGroup;
+use N1ebieski\KSEFClient\DTOs\Requests\Permissions\TargetIdentifierInternalIdGroup;
+use N1ebieski\KSEFClient\DTOs\Requests\Permissions\TargetIdentifierNipGroup;
+use N1ebieski\KSEFClient\DTOs\Requests\Permissions\TargetIdentifierTypeGroup;
+use N1ebieski\KSEFClient\Requests\AbstractRequest;
+use N1ebieski\KSEFClient\ValueObjects\Requests\Permissions\Indirect\IndirectPermissionType;
+
+final class GrantsRequest extends AbstractRequest implements BodyInterface
+{
+    /**
+     * @param array<int, IndirectPermissionType> $permissions
+     */
+    public function __construct(
+        public readonly SubjectIdentifierNipGroup | SubjectIdentifierPeselGroup | SubjectIdentifierFingerprintGroup $subjectIdentifierGroup,
+        public readonly TargetIdentifierNipGroup | TargetIdentifierInternalIdGroup | TargetIdentifierTypeGroup $targetIdentifierGroup,
+        public readonly array $permissions,
+        public readonly string $description
+    ) {
+    }
+
+    public function toBody(): array
+    {
+        /** @var array<string, mixed> $data */
+        $data = $this->toArray();
+
+        return [
+            ...$data,
+            'subjectIdentifier' => [
+                'type' => $this->subjectIdentifierGroup->getIdentifier()->getType(),
+                'value' => (string) $this->subjectIdentifierGroup->getIdentifier(),
+            ],
+            'targetIdentifier' => [
+                'type' => $this->targetIdentifierGroup->getIdentifier()->getType(),
+                ...( ! $this->targetIdentifierGroup instanceof TargetIdentifierTypeGroup ? [
+                    'value' => (string) $this->targetIdentifierGroup->getIdentifier()
+                ] : []),
+            ],
+        ];
+    }
+}
